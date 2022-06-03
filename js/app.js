fc3e8fce7edeec5e3804d9c6a2d491def6e5887c
AndyL94 <andy.louis94@outlook.fr>
2022-06-03 13:13:20 +0200

    Update

--- js/app.js
+++ js/app.js
@@ -56,6 +56,24 @@ document.addEventListener("DOMContentLoaded", e => {
     }
     window.addEventListener('scroll', scrollActive);
 
+    /*============== ACCORDION SKILLS ==============*/
+    const skillsContent = document.getElementsByClassName('skills_content'),
+          skillsHeader = document.querySelectorAll('.skills_header');
+
+    function toggleSkills() {
+        let itemClass = this.parentNode.className;
+
+        for (i = 0; i < skillsContent.length; i++) {
+            skillsContent[i].className = 'skills_content skills_close';
+        }
+        if (itemClass === 'skills_content skills_close') {
+            this.parentNode.className = 'skills_content skills_open';
+        }
+    }
+    skillsHeader.forEach(el => {
+        el.addEventListener('click', toggleSkills);
+    });
+
     /*==================== Changer le fond du header  ====================*/
     function scrollHeader() {
         const nav = document.getElementById('header');
